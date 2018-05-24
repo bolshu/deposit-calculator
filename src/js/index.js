1d9a568e29713b344bead910a93370118cfcf4cc
bolshu <vadimakulin@gmail.com>
2018-05-25 00:23:52 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -2,26 +2,36 @@ require('../scss/styles.scss');
 
 'use strict';
 
-const cashNumber = document.getElementById('cash-number');
-const percentNumber = document.getElementById('percent-number');
-const monthNumber = document.getElementById('month-number');
-const refillNumber = document.getElementById('refill-number');
+const cashRange = document.getElementById('cash-range');
+const percentRange = document.getElementById('percent-range');
+const monthRange = document.getElementById('month-range');
+const refillNumber = document.getElementById('refill-range');
 
 const profitPercent = document.getElementById('profit-percent');
 const profitCash = document.getElementById('profit-cash');
 const profitSum = document.getElementById('profit-sum');
+const profitMonth = document.getElementById('profit-month');
 
 const numbersCollection = document.querySelectorAll('input[type="number"]');
 const rangeCollection = document.querySelectorAll('input[type="range"]');
 
 const inputValue = {
-    rangeHandler : function () {
-        this.previousElementSibling.value = this.value;
+    rangeHandler: function() {
+        this.previousElementSibling.setAttribute('type', 'text');
+        this.previousElementSibling.value = this.value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
     },
-    numberHandler : function () {
-        this.nextElementSibling.value = this.value;
+    numberHandler: function() {
+        this.nextElementSibling.value = +this.value;
     },
-    valueLimit : function () {
+    numberToNumber: function() {
+        this.setAttribute('type', 'number');
+        this.value = this.nextElementSibling.value;
+    },
+    numberToString: function() {
+        this.setAttribute('type', 'text');
+        this.value = this.value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+    },
+    valueLimit: function() {
         let minVal = this.getAttribute('min');
         let maxVal = this.getAttribute('max');
         if (this.value < +minVal) {
@@ -34,20 +44,31 @@ const inputValue = {
 };
 
 function calculateTotalSum() {
-    let calcProfitCash = (cashNumber.value * (percentNumber.value / 100) / 12) * monthNumber.value,
-        calcProfitPercent = calcProfitCash * 100 / cashNumber.value,
-        calcProfitTotal = (+calcProfitCash + +cashNumber.value).toFixed(0)
-        // ,
-        // calcRefillCash;
-        // for (let i = 0; i < monthNumber.value; i++) {
-        //     return calcRefillCash = refillNumber.value * i;
-        // }
+    let calcProfitCash = (cashRange.value * (percentRange.value / 100) / 12) * monthRange.value,
+        calcProfitPercent = calcProfitCash * 100 / cashRange.value,
+        calcProfitTotal = (+calcProfitCash + +cashRange.value).toFixed(0),
+        monthValue = +monthRange.value;
     profitPercent.innerHTML = calcProfitPercent.toFixed(2);
-    profitCash.innerHTML = calcProfitCash.toFixed(0).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1\&nbsp\;');
-    profitSum.innerHTML = calcProfitTotal.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1\&nbsp\;');
+    profitCash.innerHTML = calcProfitCash.toFixed(0).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+    profitSum.innerHTML = calcProfitTotal.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+
+    if (monthValue < 5 || monthValue > 21) {
+        profitMonth.innerHTML = monthValue + ' месяца';
+    } else if (monthValue === 21) {
+        profitMonth.innerHTML = monthValue + ' месяц';
+    } else {
+        profitMonth.innerHTML = monthValue + ' месяцев';
+    }
 }
 
 calculateTotalSum();
+
+document.addEventListener('DOMContentLoaded', function() {
+    for (let i = 0; i < numbersCollection.length; i++) {
+        numbersCollection[i].setAttribute('type', 'text');
+        numbersCollection[i].value = numbersCollection[i].value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+    }
+});
 
 for (let i = 0; i < rangeCollection.length; i++) {
     rangeCollection[i].addEventListener('input', inputValue.rangeHandler);
@@ -55,11 +76,11 @@ for (let i = 0; i < rangeCollection.length; i++) {
 }
 
 for (let i = 0; i < numbersCollection.length; i++) {
-    numbersCollection[i].addEventListener('change', inputValue.valueLimit);
     numbersCollection[i].addEventListener('change', inputValue.numberHandler);
+    numbersCollection[i].addEventListener('change', inputValue.valueLimit);
     numbersCollection[i].addEventListener('change', calculateTotalSum);
-    numbersCollection[i].addEventListener('change', function() {
-        let val = this.value.toString();
-        this.innerHTML = val.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1\&nbsp\;');
-    });
+    numbersCollection[i].addEventListener('blur', inputValue.numberToString);
+    numbersCollection[i].addEventListener('focus', inputValue.numberToNumber);
 }
+
+
